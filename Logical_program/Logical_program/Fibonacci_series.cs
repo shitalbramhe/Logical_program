22cd7dda71effc4581b9f29324da72b8efd73b59
shital bramhe <shitalbramhe@gmail.com>
2021-11-11 18:09:00 +0530

    [shital] Add : initial

---
+++ Logical_program/Logical_program/Fibonacci_series.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_program
+{
+    public class Fibonacci_series
+    {
+        int first = 0, second = 1, third = 0;
+        public void Fibo(int n)
+        {
+            if (n == 0)
+            {
+                Console.WriteLine(first);
+                return;
+            }
+             for(int i=0; i<n; i++)
+            {
+                if(i==0)
+                {
+                    Console.WriteLine(first);
+                }
+                if(i== 1)
+                {
+                    Console.WriteLine(second);
+                }
+                if( i>1)
+                {
+                    third = first + second;
+                    first = second;
+                    second = third;
+                    Console.WriteLine(third);
+                }
+            }
+        }
+    }
+}
---
+++ Logical_program/Logical_program/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Logical_program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Progran number to get excuted \n1. Fibonacci series \n 2. Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            bool flag = true;
+            while (flag)
+            {
+                switch (option)
+                {
+                    case 1:
+                        Fibonacci_series Fibonacci = new Fibonacci_series();
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        Fibonacci.Fibo(number);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
